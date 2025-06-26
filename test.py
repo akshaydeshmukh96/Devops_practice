a3a3ae465d2ff766d34d36be595a46eacb9f0127
Super User <root@ip-172-31-7-80.ap-south-1.compute.internal>
2025-06-26 14:19:32 +0000

    Changes reverted

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-#"""This module demonstrates basic Python functionality."""
-#Print prime numbers from 1 to 50
+"""This module demonstrates basic Python functionality."""
 for num in range(2, 51):
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
